c7bbb4408e0f35e0d85c5ec1cabb62ff1fda1957
jlin231 <jonlin981@gmail.com>
2022-10-18 21:57:16 -0700

    first commit

--- classes/dragon.js
+++ classes/dragon.js
@@ -3,6 +3,28 @@
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
+class Dragon {
+  constructor(name, color){
+    this.name = name;
+    this.color = color;
+  }
+
+  breathesFire(){
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+  static getDragons(...dragons){
+    let output = []
+    dragons.forEach((dragon)=>{
+      output.push(dragon.name)
+    })
+    return output;
+  }
+}
+// const puff = new Dragon("Puff", "green");
+// console.log(puff);
+// console.log(puff.breathesFire());
+
 try {
   module.exports = Dragon;
 } catch {
--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,23 @@
 // Your code here
+let Dragon = require('./dragon')
+
+class FriendlyDragon extends Dragon{
+    constructor(name, color, lifeGoals, friend){
+        super(name, color);
+        this.lifeGoals = lifeGoals; //array of goals
+        this.friend = friend; //best dragon buddy
+    }
+
+    hasLifeGoals(){
+        this.lifeGoals.forEach((skill) =>{
+            console.log(`${this.name} likes to ${skill}`)}
+        )
+    }
+
+    helpsPeople(){
+        return `${this.name} helps their friend ${this.friend}`;
+    }
+}
+
+
+module.exports = FriendlyDragon;
